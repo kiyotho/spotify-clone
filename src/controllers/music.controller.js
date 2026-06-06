42d9ffc3bd550c8d9c7f32eced0b86cebfe72c84
Kiyotho <somayojha2@gmail.com>
2026-06-06 10:15:31 +0530

    used a more secure way to deal with decoded not defined bug

--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -8,35 +8,35 @@ export async function createMusicController(req, res) {
     const token = req.cookies.token
 
     if(!token) return res.status(401).json({ message: "unauthorized"})
-    
-    
-    let decoded
 
     try{
 
-        decoded = jwt.verify( token, process.env.JWT_SECRET )
-        console.log(decoded)
+            const decoded = jwt.verify( token, process.env.JWT_SECRET )
+            console.log(decoded)
+
+    
+
+        if( decoded.role !== "artist" ) return res.status(401).json({ message: "unauthorized"})
+
+        const { title, description } = req.body
+        const file = req.file
+
+        const uploadedFile = await uploadFile(file.buffer.toString("base64"))
+        console.log(uploadedFile)
+
+        const uploadedMusic = await musicModel.create({
+            uri: uploadedFile.url, 
+            title: title, 
+            description: description, 
+            artist: decoded.id
+        })
+
+        res.status(201).json({message: "success", content: uploadedMusic})
 
     }catch(err){
+
         return res.status(401).json({message: "unauthorized"})
+        
     }
-
-    if( decoded.role !== "artist" ) return res.status(401).json({ message: "unauthorized"})
-
-    const { title, description } = req.body
-    const file = req.file
-
-    const uploadedFile = await uploadFile(file.buffer.toString("base64"))
-    console.log(uploadedFile)
-
-    const uploadedMusic = await musicModel.create({
-        uri: uploadedFile.url, 
-        title: title, 
-        description: description, 
-        artist: decoded.id
-    })
-
-    res.status(201).json({message: "success", content: uploadedMusic})
-
 
 }
