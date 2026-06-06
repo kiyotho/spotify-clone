a3d168148ca444d5db99cd9d9afe26a8ab98c740
Kiyotho <somayojha2@gmail.com>
2026-06-06 19:53:58 +0530

    applying the middle ware

--- src/routers/music.routes.js
+++ src/routers/music.routes.js
@@ -4,11 +4,11 @@ import multer from 'multer'
 import { createAlbumController } from '../controllers/music.controller.js'
 import { authArtist } from '../middlewares/auth.middleware.js'
 import { fetchAllMusicController } from '../controllers/music.controller.js'
-
+import { authUser } from '../middlewares/auth.middleware.js'
 export const musicRouter = express.Router()
 
 const uploadFile = multer({ storage: multer.memoryStorage()})
 
 musicRouter.post('/upload', authArtist,  uploadFile.single('file'), createMusicController)
 musicRouter.post('/createalbum', authArtist, createAlbumController)
-musicRouter.get('/', fetchAllMusicController)
+musicRouter.get('/', authUser, fetchAllMusicController)
