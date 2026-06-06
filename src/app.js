85ab4b305e7263ff5b2020a0935d7a1891fd24fc
Kiyotho <somayojha2@gmail.com>
2026-06-06 09:29:58 +0530

    added music creation skeleton

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import cookieParser from 'cookie-parser'
 import { authRouter } from './routers/auth.routes.js'
+import { musicRouter } from './routers/music.routes.js'
 
 const app = express()
 app.use(express.json())
@@ -8,6 +9,7 @@ app.use(cookieParser())
 
 
 app.use('/api/auth', authRouter)
+app.use('api/music', musicRouter)
 
 
 export{ app }
--- src/controllers/music.controller.js
+++ src/controllers/music.controller.js
@@ -1,7 +1,24 @@
-
+import jwt from 'jsonwebtoken'
 
 
 
 export async function createMusic(req, res) {
     
+    const token = req.cookies.token
+
+    if(!token) return res.status(401).json({ message: "unauthorized"})
+    
+    try{
+
+        const decoded = jwt.verify( token, process.env.JWT_SECRET )
+
+    }catch(err){
+        res.status(401).json({message: "unauthorized"})
+    }
+
+    if( decoded.role !== "artist" ) return res.status(401).json({ message: "unauthorized"})
+
+
+
+
 }
