3e6c96c36276dac23e675fd12d4212c479f5b25d
Kiyotho <somayojha2@gmail.com>
2026-06-11 14:56:34 +0530

    made the basic ui

--- src/public/src/components/music/home.page.jsx
+++ src/public/src/components/music/home.page.jsx
@@ -12,6 +12,7 @@ export function HomePage(){
     const [currentPoint, setCurrentPoint] = useState(0)
     const [totalDuration, setTotalDuration] = useState(0)
     const [progress, setProgress] = useState(0)
+    const [volume, setVolume] = useState(0)
     const audioRef = useRef(null)
     const outerLineRef = useRef(null)
     const innerLineRef = useRef(null)
@@ -26,9 +27,6 @@ export function HomePage(){
         })
     }, [])
 
-    // useEffect(() => {
-    //     console.log(queue)
-    // }, [queue])
 
     useEffect(() => {
         const audio = audioRef.current
@@ -36,8 +34,9 @@ export function HomePage(){
         
         function updateTime(){
             if(!audio.duration) return 
-            setCurrentPoint(audio.currentTime.toFixed(2))
-            setTotalDuration(audio.duration.toFixed(2))
+
+            setCurrentPoint(Math.trunc(audio.currentTime))
+            setTotalDuration(Math.trunc(audio.duration))
             setProgress((currentPoint / totalDuration) * 100)
 
         }
@@ -51,7 +50,9 @@ export function HomePage(){
 
     return(
         <div className='app-container'>
+            
             <section className='content'>
+                <h2 style={{ color: 'white', marginBottom: '16px' }}>Songs</h2>
                 {
                     music.length > 0 ? (
                         music.map((element) => (
@@ -60,8 +61,11 @@ export function HomePage(){
                                                                                 setQueue([...queue, element])
                                                                             }
                                                                         }}>
+                                <img 
+                                    src={element.albumarturi || 'https://www.billboard.com/wp-content/uploads/2023/07/asap-rocky-long-live-asap-2013-billboard-1240.jpg?w=768'} 
+                                    className='card-img'
+                                />
                                 <p className='title'>{element.title}</p>
-                                <p>{element.description}</p>
                                 <p className='artist'>{element.artist.username}</p>
                             </div>
                         ))
@@ -69,17 +73,19 @@ export function HomePage(){
                 }
             </section>
 
+            
             <div className='player'>
-                <img src={queue[currentIndex]?.albumarturi || 'https://www.svgrepo.com/show/503126/album.svg'} width='200px'/>
-                <audio ref={audioRef} src={queue[currentIndex]?.uri} autoPlay controls onEnded={() => setCurrentIndex(currentIndex + 1)}/>
-                <div>
+                <img src={queue[currentIndex]?.albumarturi || 'https://www.billboard.com/wp-content/uploads/2023/07/asap-rocky-long-live-asap-2013-billboard-1240.jpg?w=768'} width='200px' className='discography-img'/>
+                <audio ref={audioRef} src={queue[currentIndex]?.uri} autoPlay controls onEnded={() => setCurrentIndex(currentIndex+1 === queue.length ? 0 : currentIndex + 1)}/>
+                <div className='button-container'>
                     <button onClick={() => setCurrentIndex(currentIndex - 1)}>Prev</button>
-                    <button onClick={() => setCurrentIndex(currentIndex + 1)}>Next</button>
                     <button onClick={() => audioRef.current.pause()}>Pause</button>
                     <button onClick={() => audioRef.current.play()}>Play</button>
+                    <button onClick={() => setCurrentIndex(currentIndex + 1 === queue.length ? 0 : currentIndex + 1)}>Next</button>
                 </div>
-                
-                <div>
+                  
+                <div className='slider-metadata'>
+                    <p>{Math.trunc(currentPoint/60)}:{String(Math.trunc(currentPoint%60)).padStart(2,'0')}</p>
                     <input 
                         type="range" 
                         min="0" 
@@ -91,26 +97,40 @@ export function HomePage(){
                             setProgress(e.target.value)
                         }}
                     />
-                    <p>{currentPoint}:{totalDuration}</p> 
+                    <p>{Math.trunc(totalDuration/60)}:{String(Math.trunc(totalDuration%60)).padStart(2,'0')}</p> 
                 </div>
-
-                <section>
-                    <h1>Queue</h1>
-                    {
-
-                    queue.length > 0? (
-                        queue.map((element, index ) =>(
-                            <div key={index}>
-                                <p style={{ color: index === currentIndex ? 'cyan' : 'white' }}>{element.title}</p>
-                            </div>
-                        ))
-                    ) : <h1>Queue Empty</h1>
-                    
-                    }
-                </section>
-
+                <div className='volume-adjust'>
+                    <img src='https://www.svgrepo.com/show/385213/speaker-audio-sound-loud.svg' width='30px'/>
+                    <input 
+                        type='range'
+                        min='0'
+                        max='100'
+                        value={volume}
+                        defaultValue='50'
+                        onChange={(e) =>{
+                            const newVolume = e.target.value / 100
+                            audioRef.current.volume = newVolume
+                            setVolume(e.target.value)
+                        }}
+                    />
+                </div>
+                  
+                <div>
+                    <h1 className='queue-text'>Queue</h1>
+                    <section className='queue'>
+                        {
+                            queue.length > 0? (
+                            queue.map((element, index ) =>(
+                                <div key={index}>
+                                    <p style={{ color: index === currentIndex ? 'cadetblue' : 'white' }}>{element.title}</p>
+                                </div>
+                            ))
+                        ) : <h1>Queue Empty</h1>
+                        }
+                    </section>
+                </div>
             </div>
-        
+            
         </div>
     )
 }
